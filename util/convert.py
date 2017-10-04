77f693e8ddf6f81bc2c65e8d61eb8dbf4ad11d22
Alex Biddulph <c3124185@uon.edu.au>
2017-10-04 21:28:12 +1100

    Modifies convert script to change JSON format to support videos in a saner manner and to add support for rectangles, polygons, and ellipses.

--- util/convert.py
+++ util/convert.py
@@ -1,17 +1,59 @@
 #! /usr/bin/env python
 
 import os
-files = [f for f in os.listdir('./output') if f.endswith('.txt') and f.startswith('GOPR0396')]
+import textwrap
+import json
+from progressbar import ProgressBar
+pbar = ProgressBar()
 
-for file in files:
-    video = os.path.splitext(file)[0].split('_')[0]
-    video = '"/home/bidski/ownCloud/HORUS/4TEL_Videos/VIDEOS NOV 16/GoPro/{0}.MP4"'.format(video)
+files = sorted([f for f in os.listdir('/home/bidski/ownCloud/HORUS/4TEL_Videos/VIDEOS NOV 16/GoPro/') if f.endswith('.MP4')])
 
-    with open('./json/{0}'.format(file.replace('.txt', '.json')), 'w') as out:
-        data = '[\n\t[{0}, {1},\n\t\t['.format(video, int(file.replace('.txt', '').split('_')[1]))
-        with open('./output/{0}'.format(file)) as f:
-            for line in f:
-                left, top, right, bottom = line.strip().split(' ')[4:8]
-                data = '{0}\n\t\t\t[[{1}, {2}], [{3}, {4}], [{5}, {6}], [{7}, {8}]],'.format(data,
-                    left, top, right, top, right, bottom, left, bottom)
-        out.write('{0}\n\t\t]\n\t]\n]'.format(data[:-1]))
+videos = None
+
+for file in pbar(files):
+    video = os.path.splitext(os.path.basename(file))[0]
+    video_path = '"/home/bidski/ownCloud/HORUS/4TEL_Videos/VIDEOS NOV 16/GoPro/{0}.MP4"'.format(video)
+
+    with open('./json/{0}.json'.format(video), 'w') as output:
+        output.write('[\n')
+        output.write('    "{0}",\n'.format(video))
+        output.write('    [\n')
+        output.write('        true,\n')
+        output.write('        [\n')
+
+        annotation_files = sorted([f for f in os.listdir('./output') if f.endswith('.txt') and f.startswith(video)])
+
+        for annotation_file in annotation_files:
+            with open('./output/{0}'.format(annotation_file)) as f:
+                frame_number = int(os.path.splitext(annotation_file)[0].split('_')[1])
+                output.write('            [\n')
+                output.write('                {0},\n'.format(frame_number))
+                output.write('                [\n')
+
+                lines = f.readlines()
+                for line in lines:
+                    left, top, right, bottom = line.strip().split(' ')[4:8]
+
+                    output.write('                    [\n')
+                    output.write('                        "R",\n')
+                    output.write('                        [\n')
+                    output.write('                            {0}, {1}, {2}, {3}\n'.format(left, top, right, bottom))
+                    output.write('                        ]\n')
+                    output.write('                    ]')
+
+                    if lines.index(line) != len(lines) - 1:
+                        output.write(',\n')
+                    else:
+                        output.write('\n')
+
+            output.write('                ]\n')
+            output.write('            ]')
+
+            if annotation_files.index(annotation_file) != len(annotation_files) - 1:
+                output.write(',\n')
+            else:
+                output.write('\n')
+
+        output.write('        ]\n')
+        output.write('    ]\n')
+        output.write(']\n')

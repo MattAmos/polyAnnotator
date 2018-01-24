74b2b0c342fd47c1f1c99a47700cd738e2ecdc78
MattAmos <matthewjamesamos@gmail.com>
2018-01-24 11:17:37 +1100

    Add segmentation output for empty frames

--- util/segmenter/segmenter.py
+++ util/segmenter/segmenter.py
@@ -29,7 +29,14 @@ class Segmenter:
 
 	def drawImages(self):
 		numPoly = 0; numPts = 0
+		files = os.listdir(self.currDir)
+		numFrames = len(files)
 		for f in self.polyPool["frame"]:
+			print('[{0:5d}/{1:5d}] {2}/{3:010d}.{4}'.format(f["frameNo"], numFrames, self.currDir, f["frameNo"], 'JPG'))
+			if '{0:010d}.{1}'.format(f["frameNo"], 'JPG') in files:
+				files.remove('{0:010d}.{1}'.format(f["frameNo"], 'JPG'))
+			else:
+				print('[ERR] {0} not in file list'.format('{0:010d}.{1}'.format(f["frameNo"], 'JPG')))
 			numPoly += len(f["annotation"])
 			raw = Image.open(self.currDir + "/" + '{0:010d}.{1}'.format(f["frameNo"], 'JPG'))
 			seg = Image.new('L', raw.size, 0)
@@ -49,4 +56,10 @@ class Segmenter:
 				d.polygon(dPolygon, COLOUR, 255)
 				d.line(dPolygon, 255, BORDER)
 			seg.save(self.currDir + "_seg/" + '{0:010d}.{1}'.format(f["frameNo"], 'JPG'), seg.format)
+
+		for file in files:
+			raw = Image.open(self.currDir + "/" + file)
+			seg = Image.new('L', raw.size, 0)
+			seg.save(self.currDir + "_seg/" + file, seg.format)
+
 		print("[LOG] {} images processed, {} points drawn constituting {} polygons".format(len(self.polyPool["frame"]), numPts, numPoly))
